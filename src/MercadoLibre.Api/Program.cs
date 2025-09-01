a345dc4acaaf05d5fa776eb428788d83d79826a1
Juan Pablo Ospina <juanpa2191@hotmail.com>
2025-08-31 21:55:20 -0500

    mejoras

--- src/MercadoLibre.Api/Program.cs
+++ src/MercadoLibre.Api/Program.cs
@@ -2,11 +2,17 @@ using MercadoLibre.Api.Middleware;
 using MercadoLibre.Infrastructure.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using OpenTelemetry.Metrics;
+using System;
 using System.IO;
-using System.Reflection;
+using System.Threading.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,6 +34,25 @@ builder.Services.AddSwaggerGen(c =>
     });
 });
 builder.Services.AddInfrastructureServices();
+builder.Services.AddMemoryCache();
+builder.Services.AddRateLimiter(options =>
+{
+    options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(context =>
+        RateLimitPartition.GetFixedWindowLimiter(
+            partitionKey: context.User.Identity?.Name ?? context.Request.Headers.Host.ToString(),
+            factory: partition => new FixedWindowRateLimiterOptions
+            {
+                AutoReplenishment = true,
+                PermitLimit = 100,
+                Window = TimeSpan.FromMinutes(1)
+            }));
+});
+
+builder.Services.AddResponseCompression(options =>
+{
+    options.EnableForHttps = true;
+    options.Providers.Add<GzipCompressionProvider>();
+});
 
 // Configure CORS
 builder.Services.AddCors(options =>
@@ -39,6 +64,33 @@ builder.Services.AddCors(options =>
                    .AllowAnyMethod()
                    .AllowAnyHeader();
         });
+});
+
+builder.Services.AddHealthChecks()
+    .AddCheck("FileSystem", () =>
+    {
+        try
+        {
+            File.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "products.json"));
+            return HealthCheckResult.Healthy();
+        }
+        catch
+        {
+            return HealthCheckResult.Unhealthy();
+        }
+    });
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(metrics =>
+    {
+        metrics.AddAspNetCoreInstrumentation()
+               .AddMeter("Microsoft.AspNetCore.Hosting")
+               .AddMeter("Microsoft.AspNetCore.Server.Kestrel");
+    });
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ReportApiVersions = true;
 });
 
 var app = builder.Build();
